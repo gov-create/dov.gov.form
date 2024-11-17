012a161edfd8e3f714f6c8abaa131e423eeb7a54
gov-create <airdropdev680@gmail.com>
2024-11-17 12:29:38 +0100

    Update index.js basename was changed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-const basename = '/Depaul-Uni-Exam-Form-Pay';
+const basename = '/dol.gov.form';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
